3c7797df4c69d20137a2c3611e3c1cdece569cb7
Liv <edwardghostiie@gmail.com>
2026-03-24 20:25:01 -0300

    service

--- src/main/java/liv/duda23/Service/AutoresService.java
+++ src/main/java/liv/duda23/Service/AutoresService.java
@@ -1,4 +1,35 @@
 package liv.duda23.Service;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import liv.duda23.Model.AutoresModel;
+import liv.duda23.Repository.AutorRepository;
+
+import java.util.List;
+
+@Service
 public class AutoresService {
+
+    @Autowired
+    private AutorRepository repository;
+
+    public AutoresModel criar(AutoresModel nacionalidade){
+        return repository.save(nacionalidade);
+    }
+
+    public List<AutoresModel> listar(){
+        return repository.findAll();
+    }
+
+    public AutoresModel buscar(long id){
+        return repository.findById(id).get();
+    }
+
+    public void deletar(Long id){
+        repository.deleteById(id);
+    }
+
 }
+
+
