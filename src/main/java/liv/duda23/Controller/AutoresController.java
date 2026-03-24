8040deacec7c14337020659b72cc179e9427619b
Liv <edwardghostiie@gmail.com>
2026-03-24 20:26:44 -0300

    controller

--- src/main/java/liv/duda23/Controller/AutoresController.java
+++ src/main/java/liv/duda23/Controller/AutoresController.java
@@ -1,4 +1,37 @@
 package liv.duda23.Controller;
 
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import liv.duda23.Model.AutoresModel;
+import liv.duda23.Service.AutoresService;
+
+import java.util.List;
+@RestController
+
+@RequestMapping(path = "/nacionalidade")
 public class AutoresController {
+
+    @Autowired
+    private AutoresService nacionalidadeService;
+
+    @PostMapping
+    public AutoresModel criar(@RequestBody AutoresModel nacionalidadeModel){
+        return nacionalidadeService.criar(nacionalidadeModel);
+    }
+
+    @GetMapping
+    public List<AutoresModel> listar(){
+        return nacionalidadeService.listar();
+    }
+
+    @DeleteMapping
+    public void deletar(@PathVariable Long id){
+        nacionalidadeService.deletar(id);
+    }
+
 }
+
+
+
